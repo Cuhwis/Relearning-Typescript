e5a3736cb6eb631a4c381a5d23f650afcd14b939
ChrisRuaboro <chrisruaboro@gmail.com>
2025-08-30 11:22:04 -0700

    new lesson arrays

---
+++ lessons/002-Arrays/src/index.ts
@@ -0,0 +1,29 @@
+// 002-Arrays — Arrays Introduction
+
+let customersArray = [
+	'Custy Stomer',
+	'C. Oostomar',
+	'C.U.S. Tomer',
+	3432434,
+	'Custo Mer',
+	'Custopher Ustomer',
+	3432435,
+	'Kasti Yastimeur',
+];
+
+//Write Your Code here:
+function checkCustomersArray() {
+	for (let i = 0; i < customersArray.length; i++) {
+		if (typeof customersArray[i] !== 'string') {
+			console.log(`Type error: ${customersArray[i]} should be a string!`);
+		}
+	}
+}
+
+function stringPush(val) {
+	if (typeof val === 'string') {
+		customersArray.push(val);
+	}
+}
+
+checkCustomersArray();
