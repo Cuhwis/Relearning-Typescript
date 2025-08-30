bb9af09ca6b54c6c393802758414faa085a71dc7
ChrisRuaboro <chrisruaboro@gmail.com>
2025-08-30 11:31:15 -0700

    array type annotations

---
+++ lessons/003-array-type-annotations/README.md
@@ -0,0 +1,3 @@
+# Lesson 003-array-type-annotations — array type annotations
+
+Notes go here.
---
+++ lessons/003-array-type-annotations/src/index.ts
@@ -0,0 +1,3 @@
+// 003-array-type-annotations — array type annotations
+
+console.log("Lesson 003-array-type-annotations running...");
