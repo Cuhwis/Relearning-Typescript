2a34101ab7bb1f9036e9e415e16be4605a75f3b5
ChrisRuaboro <chrisruaboro@gmail.com>
2025-09-02 21:25:02 -0700

    type aliasing

---
+++ lessons/012-type-aliases/README.md
@@ -0,0 +1,3 @@
+# Lesson 012-type-aliases — Type aliases
+
+Notes go here.
---
+++ lessons/012-type-aliases/src/index.ts
@@ -0,0 +1,10 @@
+// 012-type-aliases — Type aliases
+
+console.log('Lesson 012-type-aliases running...');
+
+// Add your type alias below:
+
+type Coord = [number, number, string, number, number, string];
+
+let codecademyCoordinates: Coord = [40, 43.2, 'N', 73, 59.8, 'W'];
+let bermudaTCoordinates: Coord = [25, 0, 'N', 71, 0, 'W'];
