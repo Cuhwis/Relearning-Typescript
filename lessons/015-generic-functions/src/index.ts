74a623522f469f79c2518842bc02cbbd6841070f
ChrisRuaboro <chrisruaboro@gmail.com>
2025-09-04 15:00:52 -0700

    generic functions was tricky

--- lessons/015-generic-functions/src/index.ts
+++ lessons/015-generic-functions/src/index.ts
@@ -1,3 +1,22 @@
 // 015-generic-functions — Generic Function Practice
 
-console.log("Lesson 015-generic-functions running...");
+console.log('Lesson 015-generic-functions running...');
+
+function getFilledArray<T>(value: T, n: number): T[] {
+	return Array(n).fill(value);
+}
+
+let stringArray: string[];
+let numberArray: number[];
+let personArray: { name: string; age: number }[];
+let coordinateArray: [number, number][];
+
+// Write your code below:
+
+stringArray = getFilledArray<string>('hi', 6);
+numberArray = getFilledArray<number>(9, 6);
+personArray = getFilledArray<{ name: string; age: number }>(
+	{ name: 'J. Dean', age: 24 },
+	6
+);
+coordinateArray = getFilledArray<[number, number]>([3, 4], 6);
