ebe9fc1b5c20cd56c4109bc09aa333f1b1eef8f4
ChrisRuaboro <chrisruaboro@gmail.com>
2025-09-11 13:59:12 -0700

    common key value pairs

--- lessons/021-common-key-value-pairs/src/index.ts
+++ lessons/021-common-key-value-pairs/src/index.ts
@@ -1,3 +1,26 @@
 // 021-common-key-value-pairs — common key value pairs
 
-console.log("Lesson 021-common-key-value-pairs: common key value pairs");
+console.log('Lesson 021-common-key-value-pairs: common key value pairs');
+
+type Like = {
+	username: string;
+	displayName: string;
+};
+
+type Share = {
+	username: string;
+	displayName: string;
+};
+
+function getFriendNameFromEvent(event: Like | Share) {
+	return event.displayName || event.username;
+}
+
+const newEvent = {
+	username: 'vkrauss',
+	displayName: 'Veronica Krauss',
+};
+
+const friendName = getFriendNameFromEvent(newEvent);
+
+console.log(`You have an update from ${friendName}.`);
