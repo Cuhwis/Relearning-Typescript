9d6e0dc80fbcecb118cbdf74f9085923143e5d06
ChrisRuaboro <chrisruaboro@gmail.com>
2025-09-14 19:15:23 -0700

    type guards lesson

---
+++ lessons/024-type-guards/README.md
@@ -0,0 +1,3 @@
+# Lesson 024-type-guards: type guards
+
+Notes go here.
---
+++ lessons/024-type-guards/src/index.ts
@@ -0,0 +1,11 @@
+// 024-type-guards — type guards
+
+console.log('Lesson 024-type-guards: type guards');
+
+function formatStatistic(stat: string | number) {
+	if (typeof stat === 'number') return stat.toFixed(2);
+	if (typeof stat === 'string') return stat.toUpperCase();
+}
+
+console.log(formatStatistic('Win'));
+console.log(formatStatistic(0.364));
