cd4367080748bec8090735cbc010c9adeb2a664a
ChrisRuaboro <chrisruaboro@gmail.com>
2025-09-25 17:33:28 -0700

    autonomous car exercise

---
+++ lessons/033-autonomous-car-with-interfaces/README.md
@@ -0,0 +1,3 @@
+# Lesson 033-autonomous-car-with-interfaces: autonomous car with interfaces
+
+Notes go here.
---
+++ lessons/033-autonomous-car-with-interfaces/src/index.ts
@@ -0,0 +1,63 @@
+// 033-autonomous-car-with-interfaces — autonomous car with interfaces
+
+console.log(
+	'Lesson 033-autonomous-car-with-interfaces: autonomous car with interfaces'
+);
+
+import { getObstacleEvents } from './computer-vision';
+
+interface AutonomousCar {
+	isRunning?: boolean;
+	respond: (events: Events) => void;
+}
+interface AutonomousCarProps {
+	isRunning?: boolean;
+	steeringControl: Steering;
+}
+interface Events {
+	[event: string]: boolean;
+}
+interface Control {
+	execute: (command: string) => void;
+}
+interface Steering extends Control {
+	turn: (direction: string) => void;
+}
+
+class SteeringControl implements Steering {
+	execute(command: string) {
+		console.log(`Executing: ${command}`);
+	}
+	turn(direction: string) {
+		this.execute(`turn ${direction}`);
+	}
+}
+
+class Car implements AutonomousCar {
+	isRunning;
+	steeringControl;
+	constructor(props: AutonomousCarProps) {
+		this.isRunning = props.isRunning;
+		this.steeringControl = props.steeringControl;
+	}
+	respond(events: Events) {
+		if (!this.isRunning) {
+			return console.log(`the car is off`);
+		}
+		Object.keys(events).forEach((eventKey) => {
+			if (!events[eventKey]) {
+				return;
+			}
+			if (eventKey === 'ObstacleLeft') {
+				this.steeringControl.turn(`right`);
+			}
+			if (eventKey === 'ObstacleRight') {
+				this.steeringControl.turn(`left`);
+			}
+		});
+	}
+}
+const steering = new SteeringControl();
+steering.turn(`left`);
+const autonomousCar = new Car({ isRunning: true, steeringControl: steering });
+autonomousCar.respond(getObstacleEvents());
